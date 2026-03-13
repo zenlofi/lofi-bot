b200b44b5a37d261e75a9400240b9c4dd6563ced
zenlofi <zen.lofi.375@gmail.com>
2026-03-13 13:44:24 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+
+while True:
+    print("Bot Zen Lofi rodando...")
+    
+    # aqui vai entrar:
+    # gerar musica
+    # juntar audio
+    # criar video
+    # subir youtube
+    
+    time.sleep(3600)
